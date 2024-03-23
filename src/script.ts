48417cf3992346c9f829ba0d238853399ee73316
MATHEUS STEFANI DOS SANTOS <41953@eep.br>
2024-03-23 15:39:51 -0300

    Botao Editar e Botao Deletar.

--- src/script.ts
+++ src/script.ts
@@ -6,8 +6,83 @@ const pegarDivTextos = document.getElementById("Colocar_textos") as HTMLDivEleme
 
 const pegarBotao = document.getElementById("botao") as HTMLButtonElement;
 
+enum NomeClasses{
+    btnDeletar = "_botaoDeletar",
+    btnEditar = "_botaoEditar",
+    btnSalvar = "_botaoSalvar",
+}
+
 pegarDivTextos.addEventListener('click',(e)=>{
-    console.log(e.target);
+    if(!e.target) return; 
+    const pegarElemento = e.target as HTMLElement;
+    
+    if(!pegarElemento.parentNode) return;
+
+    if(pegarElemento.className == NomeClasses.btnDeletar) {
+        pegarDivTextos.removeChild(pegarElemento.parentNode); 
+        return;
+    }
+
+    if(pegarElemento.className == NomeClasses.btnEditar){
+
+        const nome = pegarElemento.parentNode.childNodes[2];
+        const email = pegarElemento.parentNode.childNodes[5];
+        const idade = pegarElemento.parentNode.childNodes[8];
+
+        const nomeInput = document.createElement("input") as HTMLInputElement;
+        const emailInput = document.createElement("input") as HTMLInputElement;
+        const idadeInput = document.createElement("input") as HTMLInputElement;
+
+        if(!nome.textContent || !email.textContent || !idade.textContent)return;
+
+        nomeInput.value = nome.textContent;
+        emailInput.value = email.textContent;
+        idadeInput.value = idade.textContent;
+
+
+
+        pegarElemento.parentNode.replaceChild(nomeInput,nome);
+        pegarElemento.parentNode.replaceChild(emailInput,email);
+        pegarElemento.parentNode.replaceChild(idadeInput,idade);
+
+        const pegarBotao = pegarElemento.parentNode.childNodes[10] as HTMLButtonElement;
+
+        pegarBotao.textContent = "Salvar";
+        pegarBotao.classList.remove(NomeClasses.btnEditar);
+        pegarBotao.classList.add(NomeClasses.btnSalvar);
+
+
+        return;
+        
+        
+    }
+    if(pegarElemento.className == NomeClasses.btnSalvar){
+        
+        const criarPNomeSalvar = document.createElement("label") as HTMLLabelElement;
+        const criarPEmailSalvar = document.createElement("label") as HTMLLabelElement;
+        const criarPIdadeSalvar = document.createElement("label") as HTMLLabelElement;
+
+        const nomeSalvar = pegarElemento.parentNode.childNodes[2] as HTMLInputElement;
+        const emailSalvar = pegarElemento.parentNode.childNodes[5] as HTMLInputElement;
+        const idadeSalvar = pegarElemento.parentNode.childNodes[8] as HTMLInputElement;
+
+        criarPNomeSalvar.textContent = nomeSalvar.value;
+        criarPEmailSalvar.textContent = emailSalvar.value;
+        criarPIdadeSalvar.textContent = idadeSalvar.value;
+
+        pegarElemento.parentNode.replaceChild(criarPNomeSalvar,nomeSalvar);
+        pegarElemento.parentNode.replaceChild(criarPEmailSalvar,emailSalvar);
+        pegarElemento.parentNode.replaceChild(criarPIdadeSalvar,idadeSalvar);
+
+        const pegarBotao = pegarElemento.parentNode.childNodes[10] as HTMLButtonElement;
+
+        pegarBotao.textContent = "Editar";
+        pegarBotao.classList.remove(NomeClasses.btnSalvar);
+        pegarBotao.classList.add(NomeClasses.btnEditar);
+
+        return;
+    }
+
 })
 
 
@@ -21,9 +96,9 @@ function criarObjeto(): HTMLDivElement{
 
     let criarDiv = document.createElement("div")  as HTMLDivElement;
 
-    const criarPNome = document.createElement("p") as HTMLParagraphElement;
-    const criarPEmail = document.createElement("p") as HTMLParagraphElement;
-    const criarPIdade = document.createElement("p") as HTMLParagraphElement;
+    const criarPNome = document.createElement("label") as HTMLLabelElement;
+    const criarPEmail = document.createElement("label") as HTMLLabelElement;
+    const criarPIdade = document.createElement("label") as HTMLLabelElement;
     const criarId = document.createElement("p") as HTMLParagraphElement;
 
     const botaoEditar = document.createElement("button") as HTMLButtonElement;
@@ -49,6 +124,7 @@ function criarObjeto(): HTMLDivElement{
 
 function pegarId():string {
     const pegarUltimoFilho = pegarDivTextos.lastChild;
+
     if(pegarUltimoFilho != null && pegarUltimoFilho.firstChild != null){
 
         return String(Number(pegarUltimoFilho.firstChild.textContent)+1);
@@ -58,24 +134,43 @@ function pegarId():string {
     return  "1";
 }
 
-function adicionarFilhos(criarDiv: HTMLDivElement, criarId:HTMLParagraphElement,criarPNome:HTMLParagraphElement
-    ,criarPEmail:HTMLParagraphElement,criarPIdade:HTMLParagraphElement,
+function adicionarFilhos(criarDiv: HTMLDivElement, criarId:HTMLParagraphElement,criarPNome:HTMLLabelElement
+    ,criarPEmail:HTMLLabelElement,criarPIdade:HTMLLabelElement,
     botaoEditar:HTMLButtonElement,botaoDeletar:HTMLButtonElement): HTMLDivElement {
 
-    criarPNome.textContent = "Nome:"+pegarInputNome.value;
-    criarPEmail.textContent = "Email:"+pegarInputEmail.value;
-    criarPIdade.textContent = "Idade:"+pegarInputIdade.value; 
+    const labelNome =  document.createElement("label") as HTMLLabelElement;
+    const labelEmail =  document.createElement("label") as HTMLLabelElement;
+    const labelIdade =  document.createElement("label") as HTMLLabelElement;
+    const pularLinha1 = document.createElement("br") as HTMLBRElement; 
+    const pularLinha2 = document.createElement("br") as HTMLBRElement; 
+    const pularLinha3 = document.createElement("br") as HTMLBRElement; 
+
+    labelNome.textContent = "Nome:";
+    labelEmail.textContent = "Email:";
+    labelIdade.textContent = "Idade:";
+
+
+    criarPNome.textContent = pegarInputNome.value;
+    criarPEmail.textContent = pegarInputEmail.value;
+    criarPIdade.textContent = pegarInputIdade.value; 
+
     botaoEditar.textContent = "Editar";
     botaoDeletar.textContent = "Deletar";
     
 
-    botaoDeletar.classList.add(pegarId()+"_botaoDeletar");
-    botaoEditar.classList.add(pegarId()+"_botaoEditar");
+    botaoDeletar.classList.add("_botaoDeletar");
+    botaoEditar.classList.add("_botaoEditar");
 
     criarDiv.appendChild(criarId);
+    criarDiv.appendChild(labelNome);
     criarDiv.appendChild(criarPNome);
+    criarDiv.appendChild(pularLinha1);
+    criarDiv.appendChild(labelEmail);
     criarDiv.appendChild(criarPEmail);
+    criarDiv.appendChild(pularLinha2);
+    criarDiv.appendChild(labelIdade);
     criarDiv.appendChild(criarPIdade);
+    criarDiv.appendChild(pularLinha3);
     criarDiv.appendChild(botaoEditar);
     criarDiv.appendChild(botaoDeletar);
 
